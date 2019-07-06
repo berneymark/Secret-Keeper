036507465666e4d6c66631da4de6430620cddfd7
Mark Berney <berneydwmark@gmail.com>
2019-07-06 17:35:12 +1200

    added basic UI elements and SecurityCheck init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Camera from './jscomponents/Camera.js'
+import Camera from './jscomponents/Camera.js';
+import UserFace from './img/userFace.JPG';
 
 function App() {
   return (
     <div className="App">
+      <h1>Facial Recognition</h1>
       <Camera/>
+      <h3>Expected User</h3>
+      <img src={UserFace} alt="User Face" width="200px"/>
     </div>
   );
 }
---
+++ src/jscomponents/SecurityCheck.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class SecurityCheck {
+    constructor(props) {
+        super(props);
+        
+    }
+}
